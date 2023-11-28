36c04ffc59cb4a69a3187bd797d2229e8e2eb583
mariiakhairi <mariia_khairi@inbox.ru>
2023-11-28 11:13:56 +0300

    add second test

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -5,4 +5,9 @@ describe('Demo tests', () => {
 		cy.visit('https://example.com/')
 		cy.url().should('contain', 'example.com')
 	})
+
+	it('Assert Title', () => {
+		cy.visit('https://example.com/')
+		cy.title().should('contain', 'Example Domain')
+	})
 })
