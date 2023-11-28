517f4c3fbf7b16faa0da104052772a511ff9144e
mariiakhairi <mariia_khairi@inbox.ru>
2023-11-28 13:59:59 +0300

    refactor forms

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -11,22 +11,22 @@ describe('Forms, Buttons & Checkbox', () => {
 
 		//Load the form
 		cy.visit('https://practice-automation.com/form-fields/')
-		cy.get('[data-test="contact-form"]')
+		cy.getByTestData('contact-form')
 
 		//Fill the form
-		cy.get('#g1103-name').type(name)
+		cy.getById('g1103-name').type(name)
 		cy.contains(drink).click()
 		cy.contains(color).click()
-		cy.get('#email').type(email)
-		cy.get('#contact-form-comment-message').type(message)
+		cy.getById('email').type(email)
+		cy.getById('contact-form-comment-message').type(message)
 		cy.contains('Submit').click()
 
 		//Assert form details
-		cy.get('.contact-form-submission').should('be.visible')
-		cy.get('.field-value').contains(name).should('be.visible')
-		cy.get('.field-value').contains(drink).should('be.visible')
-		cy.get('.field-value').contains(color).should('be.visible')
-		cy.get('.field-value').contains(email).should('be.visible')
-		cy.get('.field-value').contains(message).should('be.visible')
+		cy.getByClass('contact-form-submission').should('be.visible')
+		cy.getByClass('field-value').contains(name).should('be.visible')
+		cy.getByClass('field-value').contains(drink).should('be.visible')
+		cy.getByClass('field-value').contains(color).should('be.visible')
+		cy.getByClass('field-value').contains(email).should('be.visible')
+		cy.getByClass('field-value').contains(message).should('be.visible')
 	})
 })
