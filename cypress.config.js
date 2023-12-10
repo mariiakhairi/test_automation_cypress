c2d8a981fa14ec57d58f5fa982f60172eb2eb721
mariiakhairi <mariia_khairi@inbox.ru>
2023-12-10 21:16:19 +0300

    adding configuration

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,16 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
 	reporter: 'cypress-mochawesome-reporter',
+	chromeWebSecurity: false,
+	viewportHeight: 600,
+	viewportWidth: 1000,
+	waitForAnimations: true,
+	animationDistanceThreshold: 20,
+	defaultCommandTimeout: 6000,
+	execTimeout: 60000,
+	pageLoadTimeout: 60000,
+	requestTimeout: 15000,
+	responseTimeout: 15000,
 	e2e: {
 		setupNodeEvents(on, config) {
 			require('cypress-mochawesome-reporter/plugin')(on)
